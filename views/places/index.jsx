f751c61508154ee9f24a8f3deed304fa9516721f
yournewgoodname <yournew@email.com>
2023-11-19 15:34:25 -0800

    fixing any typos

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -3,20 +3,21 @@ const Def = require('../default')
 
 function index (data) {
     let placesFormatted = data.places.map((place) => {
-        return (
-            <div>
-                <h2>{place.name}</h2>
-                <img src={place.pic} alt={place.name}>
-            </div>
-        )
+      return (
+        <div>
+          <h2>{place.name}</h2>
+          <img src={place.pic} alt={place.name}/>
+        </div>
+      )
     })
-        <Def>
-            <main>
-                <h1>PLACES INDEX PAGE</h1>
-                {placesFormatted}
-            </main>
-        </Def>
-
-}
+    return (
+      <Def>
+          <main>
+              <h1>PLACES INDEX PAGE</h1>
+              {placesFormatted}
+          </main>
+      </Def>
+  )
+  }
 
 module.exports = Def
