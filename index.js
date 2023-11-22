4c37649f061fe8985dd51817d62e633f6b81486a
yournewgoodname <yournew@email.com>
2023-11-21 20:37:49 -0800

    Adding styles to REST-rant

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+// modules
 require('dotenv').config()
 const express = require ('express')
 const app = express()
 
+// express settings
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
+// controllers and routes
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
@@ -15,4 +20,5 @@ app.get('*', (req, res) => {
     res.render('error404')
 })
 
+// listener
 app.listen(process.env.PORT)
