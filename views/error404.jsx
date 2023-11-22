03be3aa778387806022313d29784dca7884c2e99
yournewgoodname <yournew@email.com>
2023-11-21 21:03:20 -0800

    Adding Bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,12 @@ function error404 () {
             <main>
                 <h1>404: PAGE NOT FOUND</h1>
                 <p>Oops, sorry, we can't find this page!</p>
+                <div>
+                    <img src="/images/iron-man.jpg" alt="Iron Man Extending His Arm Out In A Stopping Position" />
+                    <div>
+                        Photo by <a href="https://unsplash.com/@igorbumba">Igor Bumba</a> on <a href="https://unsplash.com/photos/a-close-up-of-a-hand-with-a-light-on-it-rkaahInFlBg">Unsplash</a>
+                    </div>
+                </div>
             </main>
         </Def>
     )
--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,12 @@ function home () {
         <Def>
             <main>
                 <h1>HOME</h1>
+                <div>
+                    <img src="/images/fettucine-alfredo.jpg" alt="Fettucine Alfredo Pasta On Brown Wooden Table" />
+                    <div>
+                        Photo by: <a href="https://unsplash.com/@enginakyurt">Engin Akyurt</a> on <a href="https://unsplash.com/photos/a-plate-of-food-on-a-wooden-table-Jrvcg9My0B4">Unsplash</a>
+                    </div>
+                </div>
                 <a href="/places">
                  <button className="btn-primary">Places Page</button>
                 </a>
