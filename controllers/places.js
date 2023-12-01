437bae6affd566ddd5d1f980c4ab09d3370231bb
yournewgoodname <yournew@email.com>
2023-11-30 18:34:33 -0800

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -9,9 +9,18 @@ router.get('/', (req, res) => {
   res.render('places/index', {places})
 })
 
-// router.get('/:id', (req, res) => {
-//   res.render('/')
-// })
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', {place: places[id] })
+  }
+})
 
 router.post('/', (req, res) => {
   console.log(req.body)
